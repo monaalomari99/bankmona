a952b56d447c2f121fca4da95fc89b68ee783348
Mona Alomari <mona.99.aa@gmail.com>
2021-12-23 14:32:19 +0300

    last change

--- src/App.js
+++ src/App.js
@@ -1,76 +1,122 @@
-import {injected} from "./components/wallet/connectors"
-import {useWeb3React} from "@web3-react/core"
-import {Web3ReactProvider , getWeb3ReactConttext} from "@web3-react/core"
-import {Web3Provider} from "@ethersproject/providers"
-import {ethers,contract, Contract} from "ethers"
-import React from 'react'
+import { injected } from "./components/wallet/connectors"
+import { useWeb3React } from "@web3-react/core"
+import { Web3ReactProvider, getWeb3ReactContext } from "@web3-react/core"
+import { Web3Provider } from "@ethersproject/providers"
+import {ethers, Contract} from "ethers"
+//import formatEther from "@ethersproject/units" will want later if importing eth values probably
+import React, { useState } from 'react'
+
+export default function App() {
 
 
-export default function App(){
 
   const InteractiveArea = () => {
-    const context = useWeb3React()
-    const {chainId, provider, account, active, library, activate, deactivate} = context
-    const WalletAddress = account
-    
-    const connectwallet = () => {
-      activate(injected)
-    }
-    const disconnectWallet = () => {
-      deactivate(injected)
-    }
-    //connect to smart contract
-    const abi = [
-    ]
-    const cAddr = "0x53043A039eC996928523a3875037073dB4A2a210"
-    
-    const getReadOnlyContract = () => {
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      const contract = new Contract(cAddr,abi,provider)
-      return contract
-    }
 
-    async function getNumber() {
-      const contract = getReadOnlyContract()
-      const numberOnTract = await contract.retrieve()
-      console.log("number from contract: " + numberOnTract)
-    }
+      const [number, changeNumber] = useState(0)
+
+      function decreaseNumber() {
+        changeNumber(number - 1)
+      }
+
+      function increaseNumber() {
+        changeNumber(number + 1)
+      }
+
+      const context = useWeb3React()
+      const {chainId, provider, account, active, activate, deactivate} = context
+      const walletAddress = account
+
+      const connectWallet = () => {
+        activate(injected)
+      }
+
+      const disconnectWallet = () => {
+        deactivate(injected)
+      }
+
+      //connect to smart contract
+      const abi = [
+        {
+          "inputs": [],
+          "name": "retrieve",
+          "outputs": [
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "num",
+              "type": "uint256"
+            }
+          ],
+          "name": "store",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        }
+      ]
+      const cAddr = "0x53043A039eC996928523a3875037073dB4A2a210"
+
+      const getReadOnlyContract = () => {
+        const provider = new ethers.providers.Web3Provider(window.ethereum) //in doc
+        const contract = new Contract(cAddr, abi, provider) //in doc
+        return contract
+      }
+
+      async function getNumber() {
+        const contract = getReadOnlyContract()
+        const numberOnTract = await contract.retrieve()
+        console.log('number from contract: ' + numberOnTract)
+      }
+
+      //will require gas
+      async function storeNumber() {
+        const provider = new ethers.providers.Web3Provider(window.ethereum) //in doc
+        const signed = await provider.getSigner() //in doc
+        const contract = new Contract(cAddr, abi, signed)
+        await contract.store(number)
+      }
+
+
+
     
-    //will require gas
-    async function storeNumber() {
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      const signed = await contract.getSigner()//sign on smart contract and take gas from your wallet
-      const contract = new Contract(cAddr,abi,signed)
-      await contract.store(70)
-    }
-
-
     return(
       <div>
-         <div>
-            <button onClick = {connectwallet}>Connect</button>
-            <div>connected Address: {WalletAddress}</div>
-            <button onClick = {disconnectWallet}>Disconnect</button>
-         </div>
-         <div>
-            <button onClick = {getNumber}> Get Smart Contract Number</button>
-            <button onClick = {storeNumber}> Store Smart Contract Number</button>
-         </div>
+        <div>
+          <button onClick={connectWallet}>Connect</button>
+          <div>Connected Address: {walletAddress}</div>
+          <button onClick={disconnectWallet}>Disconnect</button>
+        </div>
+        <div>
+          <button onClick={getNumber}>Get Smart Contract Number</button>
+          <button onClick={storeNumber}>Store Smart Contract Number</button>
+        </div>
+        <div>
+          <button onClick={decreaseNumber}>Decrease</button>
+          <span>Number: {number}</span>
+          <button onClick={increaseNumber}>Increase</button>
+        </div>
       </div>
     )
   }
-  //connect to web3
-  function getLibraryf (provider) {
+ //connect to web3
+  function getLibraryf(provider) {
     const library = new Web3Provider(provider);
     library.pollingInterval = 12000;
     return library;
   }
+
   return (
-    <Web3ReactProvider getLibrary = {getLibraryf} >
+    <Web3ReactProvider getLibrary={getLibraryf}>
       <InteractiveArea />
     </Web3ReactProvider>
   )
-  
 }
-
-
